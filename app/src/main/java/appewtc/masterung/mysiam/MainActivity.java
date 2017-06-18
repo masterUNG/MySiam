fd48842a8be3e934819c91116f9180e42abf8842
masterUNG <phrombutr@gmail.com>
2017-06-18 11:41:24 +0700

    Intent to NewRegis OK

--- app/src/main/java/appewtc/masterung/mysiam/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mysiam/MainActivity.java
@@ -1,13 +1,56 @@
 package appewtc.masterung.mysiam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial View
+        initialView();
+
+        //TextView Controller
+        textViewController();
+
+
+    }   // Mathod Main
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Intent to NewRegisterActivity
+                Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+
+    }
+
+}   // Main Class นี่คือ คลาสหลัก
