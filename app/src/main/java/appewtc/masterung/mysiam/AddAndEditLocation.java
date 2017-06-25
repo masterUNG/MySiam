b3ebe1eb8ee83d1a01714a912f7aaafb70ba42b9
masterUNG <phrombutr@gmail.com>
2017-06-25 13:49:06 +0700

    Marker OK

--- app/src/main/java/appewtc/masterung/mysiam/AddAndEditLocation.java
+++ app/src/main/java/appewtc/masterung/mysiam/AddAndEditLocation.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
 /**
  * Created by masterUNG on 6/25/2017 AD.
  */
@@ -21,10 +27,23 @@ public class AddAndEditLocation extends AsyncTask<String, Void, String>{
 
         try {
 
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd", "true")
+                    .add("Name", strings[0])
+                    .add("Lat", strings[1])
+                    .add("Lng", strings[2])
+                    .build();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(strings[3]).post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
         } catch (Exception e) {
             Log.d("SiamV3", "e doin ==> " + e.toString());
+            return null;
         }
 
-        return null;
+
     }
 }   // Main Class
